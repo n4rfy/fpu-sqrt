34ec78dc70fb2ec49aa687c68168d74092811a09
n4rfy <75389781+n4rfy@users.noreply.github.com>
2021-01-08 19:34:35 -0400

    Create sqrt.asm

---
+++ sqrt.asm
@@ -0,0 +1,21 @@
+bits 64
+
+global _start
+
+section .data
+	num: dq 114.63 ; ponto flutuante de dupla precisão.
+section .bss
+	res: resq 1
+
+section .text
+
+_start:
+	fld qword [num] ; "Floating Load" (fld) trata o num como QWORD e guarda o resultado em st0.
+	fsqrt ; "Floating Square Root" (Raiz Quadrada) calcula o valor de st0, e guarda o resultado em st0.
+
+	fstp qword [res] ; guarda o valor em st (resultado) em "res" tratando
+	como QWORD.
+
+	mov eax, 1 ; 1 = exit()
+	mov ebx, 0 ; exit(0)
+	int 0x80 ; interrupção 0x80

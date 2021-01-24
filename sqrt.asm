ea4bed16f273726fad38f0eced7edfd3bf055c5a
n4rfy <75389781+n4rfy@users.noreply.github.com>
2021-01-24 14:15:37 -0400

    Update sqrt.asm

--- sqrt.asm
+++ sqrt.asm
@@ -13,7 +13,7 @@ _start:
 	fld qword [num] ; "Floating Load" (fld) trata o num como QWORD e guarda no topo da pilha.
 	fsqrt ; "Floating Square Root" (Raiz Quadrada) calcula a raiz quadrada do valor no topo da pilha, e guarda o resultado em st0.
 
-	fstp qword [res] ; guarda o valor em st (resultado) em "res" tratando como QWORD.
+	fstp qword [res] ; guarda o valor em st0 (resultado) em "res" tratando como QWORD.
 
 	; exit(0) ;
 	mov rax, 60
